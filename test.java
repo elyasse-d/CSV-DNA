42c2c4dfe462b8502117606ba29c7baf4eb45e78
EL YASSE DERROUG <40761380+elyasse-d@users.noreply.github.com>
2022-08-22 12:33:09 +0100

    update

---
+++ test.java
@@ -0,0 +1,13 @@
+import gyneDNA.*;
+
+public class test {
+
+    
+    public void Start(){
+        
+        String A = findgene("ATGGGGTAA",0);
+        System.out.println(A);
+    }
+    
+    
+}
